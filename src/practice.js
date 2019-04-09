ea87f1ec89a04b33636a41d5235d9c3b6add5051
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-04-09 10:27:33 -0400

    comments

--- src/practice.js
+++ src/practice.js
@@ -18,7 +18,6 @@ const knexInstance = knex({
     //move the connection string into an environment variable, you dont want the connect details hard coded
     connection: process.env.DB_URL
 })
-console.log('connection successful');
 
 //to start building the query, you first tell the knex instance which table you want to query
 
@@ -88,3 +87,99 @@ function searchByProduceName(searchTerm) {
 }
 
 searchByProduceName('holo')
+
+
+
+//You need to build a query that allows customers to paginate the amazong_products table products, 10 products at a time
+//use the LIMIT and OFFSET operators to achieve pagination. Limit tells us the number of items to display and offset will be the starting position in the list to count up to the limit from
+
+//we want page 4, to find the offset: we'd minus one from the page number (3), multiply this number by the limit, 10, giving us 30
+// SELECT product_id, name, price, category
+// FROM amazong_products
+// LIMIT 10
+// OFFSET 30;
+
+
+//paginateProducts function
+function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+paginateProducts(2)
+
+
+
+//You need to build a query that allows customers to filter the amazong_products table for products that have images
+//We can't use image != NULL in our query as it would return every row, in SQL NULL gets special treatment, two NULL values can't be equivalent. So, we need to use the IS NOT NULL operator
+// SELECT product_id, name, price, category, image
+// FROM amazong_products
+// WHERE image IS NOT NULL;
+
+
+//In knex, we use the .whereNotNull() method and supply a column name
+function getProductsWithImages() {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category', 'image')
+        .from('amazong_products')
+        .whereNotNull('image')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getProductsWithImages()
+
+
+//You need to build a query that allows customers to see the most popular videos by view at Whopipe by region for the last 30 days
+//query the whopipe_video_views table. Each row in this table reflects one view of a video. Each row has a region that the video was viewed and the datetime of the view. We need to count the number of views for each combination of region and video name
+//We'll use the count() function to count the dates viewed
+// We'll group our results by a combination of the video's name and region
+// We'll sort the results by the region and order by the count with most popular first
+// We need to use a WHERE to filter only results within the last 30 days
+
+//count(date_viewed) to get the number of views for each combination of the GROUP BY video_name, region. We've also renamed the count(date_viewed) to "views"
+// SELECT video_name, region, count(date_viewed) AS views
+// FROM whopipe_video_views
+
+//used now() - '30 days'::INTERVAL to calculate the datetime that's 30 days before now by using 30 days::interval from now(). Then we look for any date_viewed that is greater than 30 days before now.
+// WHERE date_viewed > (now() - '30 days':: INTERVAL)
+
+// GROUP BY video_name, region
+// ORDER BY region ASC, views DESC;
+
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+        .select('video_name', 'region')
+        //To achieve the SQL count(date_viewed) AS views we needed to use the .count method instead of listing the column in the select method
+        .count('date_viewed AS views')
+        .where(
+            'date_viewed',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+        )
+        .from('whopipe_video_views')
+        .groupBy('video_name', 'region')
+        //orderBy method allows us to "order by" multiple columns by specifying an array, we can also use objects in the array to specify different directions (ascending or descending) to "order by"
+        .orderBy([
+            { column: 'region', order: 'ASC' },
+            { column: 'views', order: 'DESC' },
+        ])
+        .then(result => {
+            console.log(result)
+        })
+}
+
+mostPopularVideosForDays(30)
+
+//Knex doesn't have methods for the now() - '30 days'::INTERVAL. Instead, knex provides a fallback method called .raw(). We can use the raw method to pass in "raw" SQL as a string.
+// An extra security measure is to tell the raw method that the raw SQL contains user input.We used ?? to tell knex that this is the position in the raw SQL that will contain user input.We then specify the value for the user input as the second argument to.raw().This is called a prepared statement
